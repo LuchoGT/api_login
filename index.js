b296a9df76d7e1d6d9a622d11dc662c30c1f9e69
Luis Gonzales <luisgonzales@synnexaconsulting.com>
2024-04-10 13:12:43 -0500

    login completo?

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
+require('dotenv').config();
+const {dbConnection}  = require('./database/config');
 const app = express();
+const routerApi = require('./routes');
+const cors = require("cors");
+// const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
+//Base de datos
+dbConnection();
+
+app.use(cors());
+
+
+app.use(express.json());
 
 app.get("/", (req, res) =>{
     res.send("Hola mi server en Express de prueba api ");
   });
   
 
-app.listen(port, () =>{
-    console.log(`Servidor corriendo en el puerto ${port}`);
+  routerApi(app);
+
+app.listen(process.env.PORT , () =>{
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
   });
   
